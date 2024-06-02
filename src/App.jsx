2f075b148186fb5702c68533a5a0fbd74c207e78
grinna0105 <111987558+grinna0105@users.noreply.github.com>
2024-06-02 13:35:27 +0300

    upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Profile from './Profile/Profile.jsx';
-import userData from '../userData.json';
-import FriendList from './FriendList/FriendList.jsx';
-import friends from '../friends.json';
-import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
-import transactions from '../transactions.json';
+import Profile from './components/Profile/Profile.jsx';
+import userData from './userData.json';
+import FriendList from './components/FriendList/FriendList.jsx';
+import friends from './friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory.jsx';
+import transactions from './transactions.json';
 
 
 export default function App() {
