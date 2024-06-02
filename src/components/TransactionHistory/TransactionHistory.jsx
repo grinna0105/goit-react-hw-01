098c7192659d5272f0294fe93b04efd31bad37bc
grinna0105 <111987558+grinna0105@users.noreply.github.com>
2024-06-02 13:06:49 +0300

    final commit

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -26,7 +26,7 @@ const TransactionHistory = ({ items }) => {
 };
 
 TransactionHistory.propTypes = {
-  item: PropTypes.arrayOf(
+  items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
